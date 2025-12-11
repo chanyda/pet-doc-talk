37be6cc960474642160f77ac58a4d3b5c854bdf0
chanyda <qkrcodms99@gmail.com>
2025-12-11 17:55:35 +0900

    Chore: helmet 패키지 설치 및 helmet 설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
 import { ConfigType } from "./types/config.type";
+import helmet from "helmet";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-
     const configService = app.get(ConfigService<ConfigType, true>);
+
+    app.use(helmet());
 
     await app.listen(configService.get("app.port", { infer: true }));
 }
