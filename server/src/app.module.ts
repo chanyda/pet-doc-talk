bfaca0d018564c2a4f9d51673ba827614f1d89ad
chanyda <qkrcodms99@gmail.com>
2025-12-11 17:41:11 +0900

    Feat: App Module에 config 설정 및 port 하드코딩 제거하고 ConfigService에서 가져와 사용하도록 변경

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from "@nestjs/common";
 import { TestController } from "./test/test.controller";
 import { TestService } from "./test/test.service";
+import { ConfigModule } from "@nestjs/config";
+import appConfig from "./config/app.config";
+import { validate } from "./config/validation/env.validation";
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            load: [appConfig],
+            envFilePath: ".env",
+            validate,
+        }),
+    ],
     controllers: [TestController],
     providers: [TestService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
+import { ConfigType } from "./types/config.type";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+
+    const configService = app.get(ConfigService<ConfigType, true>);
+
+    await app.listen(configService.get("app.port", { infer: true }));
 }
 bootstrap();
