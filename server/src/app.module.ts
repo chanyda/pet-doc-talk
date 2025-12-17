8a9b6615b2a0465957337ef2ac248cf0cd1b257d
chanyda <qkrcodms99@gmail.com>
2025-12-17 19:06:44 +0900

    Feat: app module에 prismaModule, UsersModule import 처리

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import appConfig from "./config/app.config";
 import { validate } from "./config/validation/env.validation";
+import { PrismaModule } from "./prisma/prisma.module";
+import { UsersModule } from "./users/users.module";
 import prismaConfig from "./config/prisma.config";
 
 @Module({
@@ -12,6 +14,8 @@ import prismaConfig from "./config/prisma.config";
             envFilePath: ".env",
             validate,
         }),
+        PrismaModule,
+        UsersModule,
     ],
 })
 export class AppModule {}
