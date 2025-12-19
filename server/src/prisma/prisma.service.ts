131fc7e7084e21f12be6c006cb9105656ac83c5b
chanyda <qkrcodms99@gmail.com>
2025-12-19 17:09:53 +0900

    Feat: query log 설정

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -6,9 +6,14 @@ import { PrismaClient } from "generated/prisma/client";
 @Injectable()
 export class PrismaService extends PrismaClient {
     constructor(private configService: ConfigService) {
+        const nodeEnv = configService.get<string>("app.nodeEnv");
+
         const adapter = new PrismaPg({
             connectionString: configService.get<string>("prisma.databaseUrl"),
         });
-        super({ adapter });
+        super({
+            adapter,
+            log: nodeEnv !== "production" ? ["query", "info", "warn", "error"] : [],
+        });
     }
 }
