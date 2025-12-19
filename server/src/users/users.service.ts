3792370652bd63486e6bddcc56b9b77d674ce4a5
chanyda <qkrcodms99@gmail.com>
2025-12-19 16:15:15 +0900

    Feat: user create, findByEmail, updateById service 단 코드 작성, 사용하지 않는 findAll 함수 제거

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
+import { UsersRepository } from "./users.repository";
+import { CreateUserDto } from "./dtos/create-user.dto";
+import { UpdateUserDto } from "./dtos/update-user.dto";
+import { IUser } from "./interfaces/users.interface";
 
 @Injectable()
 export class UsersService {
-    constructor(private prismaService: PrismaService) {}
+    constructor(private readonly usersRepository: UsersRepository) {}
 
-    findAll() {
-        return this.prismaService.user.findMany();
+    create(createUserDto: CreateUserDto): Promise<IUser> {
+        return this.usersRepository.create(createUserDto);
+    }
+
+    findByEmail(email: string): Promise<IUser | null> {
+        return this.usersRepository.findByEmail(email);
+    }
+
+    updateById(userId: number, updateUserDto: UpdateUserDto): Promise<IUser> {
+        return this.usersRepository.updateById(userId, updateUserDto);
     }
 }
