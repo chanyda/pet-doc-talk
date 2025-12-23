7284021cca9758895466c50297af6c38023d2baf
chanyda <qkrcodms99@gmail.com>
2025-12-23 18:33:46 +0900

    Feat: nickname nullable하지 않도록 변경, user soft delete 제거, migration처리

--- server/prisma/migrations/20251223093312_init/migration.sql
+++ server/prisma/migrations/20251223093312_init/migration.sql
@@ -12,7 +12,6 @@ CREATE TABLE "User" (
     "refreshToken" VARCHAR(500) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "deletedAt" TIMESTAMP(3),
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
