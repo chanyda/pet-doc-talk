eb27d7f51fdfd244e6c8067c6174ca08c0d59a38
chanyda <qkrcodms99@gmail.com>
2025-12-29 16:08:48 +0900

    Feat: 누락된 UserRepository interface 작성

--- server/src/users/interfaces/users.repository.interface.ts
+++ server/src/users/interfaces/users.repository.interface.ts
@@ -1,9 +1,12 @@
+import { UserSelect } from "generated/prisma/models";
 import { CreateUserDto } from "../dtos/create-user.dto";
 import { UpdateUserDto } from "../dtos/update-user.dto";
 import { IUser } from "./users.interface";
 
 export interface IUsersRepository {
     create(createUserDto: CreateUserDto): Promise<IUser>;
-    findByEmail(email: string): Promise<IUser | null>;
+    findById(id: number, select?: UserSelect): Promise<IUser | null>;
+    findByEmail(email: string, select?: UserSelect): Promise<IUser | null>;
+    findByNickname(nickname: string, select?: UserSelect): Promise<IUser | null>;
     updateById(userId: number, updateUserDto: UpdateUserDto): Promise<IUser>;
 }
