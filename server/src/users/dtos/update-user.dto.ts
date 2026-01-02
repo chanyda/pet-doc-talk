308a9ff55e239465632c3c3add964c98cf70d545
chanyda <qkrcodms99@gmail.com>
2026-01-02 14:24:25 +0900

    Refactor:  UpdateUserDto 와 UpdateProfileDto 중복 제거

--- server/src/users/dtos/update-user.dto.ts
+++ server/src/users/dtos/update-user.dto.ts
@@ -1,23 +1,10 @@
 import { ApiPropertyOptional } from "@nestjs/swagger";
-import { IsJWT, IsNotEmpty, IsOptional, IsString, IsUrl, MaxLength, MinLength } from "class-validator";
+import { IsJWT, IsOptional } from "class-validator";
+import { UpdateProfileDto } from "./update-profile-dto";
 
-export class UpdateUserDto {
-    @ApiPropertyOptional({ minLength: 1, maxLength: 20 })
-    @IsOptional()
-    @IsString()
-    @IsNotEmpty()
-    @MinLength(1)
-    @MaxLength(20)
-    nickname?: string;
-
+export class UpdateUserDto extends UpdateProfileDto {
     @ApiPropertyOptional()
     @IsOptional()
-    @IsUrl({}, { message: "profileImageUrl must be a valid URL" })
-    profileImageUrl?: string;
-
-    @ApiPropertyOptional()
-    @IsOptional()
-    @IsNotEmpty()
     @IsJWT()
     refreshToken?: string;
 }
