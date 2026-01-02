8bb9206341b1b100b80abe95310e8475164cb968
chanyda <qkrcodms99@gmail.com>
2026-01-02 14:24:48 +0900

    Feat: 사용자 프로필 업데이트 API 구현

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, HttpCode, HttpStatus, Req, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, Patch, Req, UseGuards } from "@nestjs/common";
 import { UsersService } from "./users.service";
-import { ApiBearerAuth, ApiNotFoundResponse, ApiOkResponse, ApiTags } from "@nestjs/swagger";
+import { ApiBadRequestResponse, ApiBearerAuth, ApiNotFoundResponse, ApiOkResponse, ApiTags } from "@nestjs/swagger";
 import { AuthGuard } from "src/auth/guards/auth.guard";
 import { AuthRequest } from "src/types/request.type";
 import { FindProfileResponseDto } from "./dtos/find-profile-response.dto";
+import { UpdateProfileDto } from "./dtos/update-profile-dto";
 
 @ApiBearerAuth()
 @ApiTags("users")
@@ -18,5 +19,17 @@ export class UsersController {
     @ApiNotFoundResponse({ description: "User not exists." })
     findProfile(@Req() req: AuthRequest): Promise<FindProfileResponseDto> {
         return this.usersService.findProfile(req.user.userId);
+    }
+
+    @Patch("me")
+    @HttpCode(HttpStatus.OK)
+    @ApiOkResponse({ description: "Update profile successful.", type: FindProfileResponseDto })
+    @ApiNotFoundResponse({ description: "User not exists." })
+    @ApiBadRequestResponse({ description: "This nickname is already in use." })
+    updateProfile(
+        @Req() req: AuthRequest,
+        @Body() updateProfileDto: UpdateProfileDto,
+    ): Promise<FindProfileResponseDto> {
+        return this.usersService.updateProfile(req.user.userId, updateProfileDto);
     }
 }
