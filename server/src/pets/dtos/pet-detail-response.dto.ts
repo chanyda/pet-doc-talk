28cac543d4a526f86bdec48b7e9bd3a436e4064d
chanyda <qkrcodms99@gmail.com>
2026-01-05 17:42:05 +0900

    Refactor: 펫 요약 dto를 정의하고 PetDetailResponseDto에서 extends해서 사용하도록 함

--- server/src/pets/dtos/pet-detail-response.dto.ts
+++ server/src/pets/dtos/pet-detail-response.dto.ts
@@ -1,48 +1,9 @@
-import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import { ApiPropertyOptional } from "@nestjs/swagger";
 import { Decimal } from "@prisma/client/runtime/index-browser";
-import {
-    IsBoolean,
-    IsEnum,
-    IsInt,
-    IsNotEmpty,
-    IsNumber,
-    IsOptional,
-    IsString,
-    IsUrl,
-    MaxLength,
-    MinLength,
-} from "class-validator";
-import { PetGender, PetType } from "generated/prisma/enums";
+import { IsBoolean, IsDate, IsNumber, IsOptional } from "class-validator";
+import { PetSummaryDto } from "./pet-summary.dto";
 
-export class PetDetailResponseDto {
-    @ApiProperty()
-    @IsInt()
-    id: number;
-
-    @ApiProperty({ description: "The name of pet.", minLength: 1, maxLength: 100 })
-    @IsString()
-    @MinLength(1)
-    @MaxLength(100)
-    @IsNotEmpty()
-    name: string;
-
-    @ApiProperty({ description: "The type of pet.", enum: PetType })
-    @IsEnum(PetType)
-    @IsNotEmpty()
-    type: PetType;
-
-    @ApiPropertyOptional({ description: "The gender of pet.", enum: PetGender })
-    @IsOptional()
-    @IsEnum(PetGender)
-    gender?: PetGender | null;
-
-    @ApiPropertyOptional({ type: String, description: "The breed of pet.", maxLength: 100 })
-    @IsOptional()
-    @IsString()
-    @MaxLength(100)
-    @IsNotEmpty()
-    breed: string;
-
+export class PetDetailResponseDto extends PetSummaryDto {
     @ApiPropertyOptional({ description: "The weight of pet in kg.", example: 5.85 })
     @IsOptional()
     @IsNumber({ maxDecimalPlaces: 2 })
@@ -50,16 +11,11 @@ export class PetDetailResponseDto {
 
     @ApiPropertyOptional({ type: Date, description: "The age of pet in months." })
     @IsOptional()
-    @IsInt()
+    @IsDate()
     birthDate?: Date | null;
 
     @ApiPropertyOptional({ type: Boolean, description: "Whether the pet is neutered." })
     @IsOptional()
     @IsBoolean()
     isNeutered?: boolean | null;
-
-    @ApiPropertyOptional({ type: String, description: "The image URL of pet.", maxLength: 500 })
-    @IsOptional()
-    @IsUrl()
-    imageUrl?: string | null;
 }
---
+++ server/src/pets/dtos/pet-summary.dto.ts
@@ -0,0 +1,37 @@
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
+import { IsEnum, IsInt, IsNotEmpty, IsOptional, IsString, IsUrl, MaxLength, MinLength } from "class-validator";
+import { PetGender, PetType } from "generated/prisma/enums";
+
+export class PetSummaryDto {
+    @ApiProperty()
+    @IsInt()
+    id: number;
+
+    @ApiProperty({ description: "The name of pet.", minLength: 1, maxLength: 100 })
+    @IsString()
+    @MinLength(1)
+    @MaxLength(100)
+    @IsNotEmpty()
+    name: string;
+
+    @ApiProperty({ description: "The type of pet.", enum: PetType })
+    @IsEnum(PetType)
+    @IsNotEmpty()
+    type: PetType;
+
+    @ApiProperty({ description: "The gender of pet.", enum: PetGender })
+    @IsEnum(PetGender)
+    @IsNotEmpty()
+    gender: PetGender;
+
+    @ApiProperty({ type: String, description: "The breed of pet.", maxLength: 100 })
+    @IsString()
+    @MaxLength(100)
+    @IsNotEmpty()
+    breed: string;
+
+    @ApiPropertyOptional({ type: String, description: "The image URL of pet.", maxLength: 500 })
+    @IsOptional()
+    @IsUrl()
+    imageUrl?: string | null;
+}
