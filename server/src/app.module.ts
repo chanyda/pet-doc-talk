736f12cc72f222fc7016e1a800d5599ffadf9c98
chanyda <qkrcodms99@gmail.com>
2026-01-08 15:44:07 +0900

    Test: EC2 테스트를 위해 생성했던 테스트 API 제거

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -12,7 +12,6 @@ import { ClsModule } from "nestjs-cls";
 import { ClsPluginTransactional } from "@nestjs-cls/transactional";
 import { TransactionalAdapterPrisma } from "@nestjs-cls/transactional-adapter-prisma";
 import { PrismaService } from "./prisma/prisma.service";
-import { TestModule } from "./test/test.module";
 import { PetsModule } from "./pets/pets.module";
 
 @Module({
@@ -37,7 +36,6 @@ import { PetsModule } from "./pets/pets.module";
         PrismaModule,
         UsersModule,
         AuthModule,
-        TestModule,
         PetsModule,
     ],
     providers: [AuthService],
