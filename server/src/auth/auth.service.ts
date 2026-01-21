3663523ba6879444953d67393d02c7d7bb715491
chanyda <qkrcodms99@gmail.com>
2026-01-21 17:26:27 +0900

    Fix: login시 누락된 트랜잭션 추가함

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -6,6 +6,7 @@ import { ConfigService } from "@nestjs/config";
 import { GenerateTokenResponseDto, LoginResponseDto } from "./dtos/responses/login-response.dto";
 import { ConfigType } from "src/types/config.type";
 import { nanoid } from "nanoid";
+import { Transactional } from "@nestjs-cls/transactional";
 
 @Injectable()
 export class AuthService {
@@ -15,6 +16,7 @@ export class AuthService {
         private configService: ConfigService<ConfigType, true>,
     ) {}
 
+    @Transactional()
     async login(loginDto: LoginDto): Promise<LoginResponseDto> {
         let user = await this.usersService.findByEmail(loginDto.email);
         let isNewUser = false;
