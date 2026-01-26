f4e7f81e2be7ad0390e2817ee1eb6a9e14e0f5bc
chanyda <qkrcodms99@gmail.com>
2026-01-26 20:32:18 +0900

    Feat: 소셜 로그인 후 메인페이지로 redirect되도록 하고 사용하지 않는 API는 제거함

--- server/src/auth-kakao/auth-kakao.controller.ts
+++ server/src/auth-kakao/auth-kakao.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode, HttpStatus, Query } from "@nestjs/common";
+import { Controller, Get, HttpCode, HttpStatus, Query, Res } from "@nestjs/common";
 import {
     ApiBadRequestResponse,
     ApiInternalServerErrorResponse,
@@ -10,28 +10,51 @@ import {
 import { AuthKakaoService } from "./auth-kakao.service";
 import { AuthService } from "src/auth/auth.service";
 import { LoginFrom } from "generated/prisma/enums";
-import { LoginResponseDto } from "src/auth/dtos/responses/login-response.dto";
 import { KakaoLoginQueryDto } from "./dtos/requests/kakao-login-query.dto";
+import { Response } from "express";
+import { ConfigType } from "src/types/config.type";
+import { ConfigService } from "@nestjs/config";
 
 @ApiTags("auth/kakao")
 @Controller("auth/kakao")
 export class AuthKakaoController {
+    private readonly origin: string;
+
     constructor(
         private readonly authKakaoService: AuthKakaoService,
         private readonly authService: AuthService,
-    ) {}
+        configService: ConfigService<ConfigType, true>,
+    ) {
+        this.origin = configService.getOrThrow("app.origin", { infer: true });
+    }
 
     @Get("callback")
-    @HttpCode(HttpStatus.OK)
-    @ApiOkResponse({ description: "Kakao login success.", type: LoginResponseDto })
+    @HttpCode(HttpStatus.FOUND)
+    @ApiOkResponse({ description: "Kakao login success." })
     @ApiBadRequestResponse({ description: "Invalid request to Kakao API." })
     @ApiUnauthorizedResponse({ description: "Invalid token or kakao authentication failed." })
     @ApiNotFoundResponse({ description: "Kakao account not exists or email, name not found." })
     @ApiInternalServerErrorResponse({
         description: "Failed to authenticate with Kakao OAuth service or retrieve user information from Kakao.",
     })
-    async kakaoLogin(@Query() query: KakaoLoginQueryDto): Promise<LoginResponseDto> {
+    async kakaoLogin(@Query() query: KakaoLoginQueryDto, @Res() res: Response): Promise<void> {
         const { email, name } = await this.authKakaoService.authenticate(query.code);
-        return this.authService.login({ email, name, loginFrom: LoginFrom.KAKAO });
+        const { accessToken, refreshToken } = await this.authService.login({ email, name, loginFrom: LoginFrom.KAKAO });
+
+        res.cookie("accessToken", accessToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "lax",
+            maxAge: 1000 * 60 * 60 * 24,
+        });
+
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "lax",
+            maxAge: 1000 * 60 * 60 * 24 * 30,
+        });
+
+        return res.redirect(`${this.origin}`);
     }
 }
--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,24 +1,13 @@
 import { Body, Controller, Headers, HttpCode, HttpStatus, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import { LoginDto } from "./dtos/requests/login.dto";
 import { RefreshTokenDto } from "./dtos/requests/refresh-token.dto";
-import { GenerateTokenResponseDto, LoginResponseDto } from "./dtos/responses/login-response.dto";
-import { ApiBadRequestResponse, ApiOkResponse, ApiTags, ApiUnauthorizedResponse } from "@nestjs/swagger";
+import { GenerateTokenResponseDto } from "./dtos/responses/login-response.dto";
+import { ApiOkResponse, ApiTags, ApiUnauthorizedResponse } from "@nestjs/swagger";
 
 @ApiTags("auth")
 @Controller("auth")
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
-
-    @Post("login")
-    @HttpCode(HttpStatus.OK)
-    @ApiOkResponse({ description: "Login successful.", type: LoginResponseDto })
-    @ApiBadRequestResponse({
-        description: "The user previously logged in using a different social provider.",
-    })
-    async login(@Body() loginDto: LoginDto): Promise<LoginResponseDto> {
-        return this.authService.login(loginDto);
-    }
 
     @Post("refresh")
     @HttpCode(HttpStatus.OK)
