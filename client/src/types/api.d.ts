791650506d62c22a27e7fbaf5f3cb6bd4efbcaff
chanyda <qkrcodms99@gmail.com>
2026-01-29 18:31:10 +0900

    Feat: CommentListResponse 변경에 따라 타입 수정

--- client/src/types/api.d.ts
+++ client/src/types/api.d.ts
@@ -22,6 +22,7 @@ interface CreateCommentBody {
 interface CommentListResponse {
     comments: PostComment[];
     nextCursor: number | null;
+    totalParentCommentCount: number;
     totalCommentCount: number;
 }
 interface ReplyListResponse {
