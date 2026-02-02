c8130ed3250ce3022120b2fb2c4e958a00dea70e
chanyda <qkrcodms99@gmail.com>
2026-02-02 16:16:43 +0900

    Feat: editor 사용을 위해 Tiptap 컴포넌트 생성

---
+++ client/src/components/editor/Tiptap.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+
+interface TiptapProps {
+    onChange: (html: string) => void;
+}
+
+export function Tiptap({ onChange }: TiptapProps) {
+    const handleContent = (newContent: string) => {
+        onChange(newContent);
+    };
+
+    const editor = useEditor({
+        // TODO: toolbar 생성 시 extension 추가하기
+        extensions: [StarterKit],
+        editorProps: {
+            attributes: {
+                class: "prose max-w-full text-left px-4 py-4 my-6 min-h-[300px] border border-gray-border rounded-lg focus:outline-none",
+            },
+        },
+        onUpdate: ({ editor }) => {
+            handleContent(editor.isEmpty ? "" : editor.getHTML());
+        },
+        // Don't render immediately on the server to avoid SSR issues
+        immediatelyRender: false,
+    });
+
+    return (
+        <div>
+            <EditorContent editor={editor} />
+        </div>
+    );
+}
