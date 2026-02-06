00d151fc3e60a8a5527bc4c063a292abbf4a2882
chanyda <qkrcodms99@gmail.com>
2026-02-06 16:04:00 +0900

    Feat: Consultations findMany, findManyAndCount, count 함수 생성

--- server/src/consultations/consultations.repository.ts
+++ server/src/consultations/consultations.repository.ts
@@ -2,14 +2,36 @@ import { Injectable } from "@nestjs/common";
 import { TransactionalAdapterPrisma } from "@nestjs-cls/transactional-adapter-prisma";
 import { TransactionHost } from "@nestjs-cls/transactional";
 import { PrismaService } from "src/prisma/prisma.service";
-import { IConsultationsRepository } from "./interfaces/consultations.repository.interface";
+import { FindManyAndCountResult, IConsultationsRepository } from "./interfaces/consultations.repository.interface";
 import { IConsultation } from "./interfaces/consultations.interface";
-import { ConsultationSelect } from "generated/prisma/models";
+import { ConsultationFindManyArgs, ConsultationGetPayload, ConsultationSelect } from "generated/prisma/models";
+import { ConsultationWhereInput, SelectSubset } from "generated/prisma/internal/prismaNamespace";
 import { CreateConsultationDto } from "./dtos/requests/create-consultation.dto";
 
 @Injectable()
 export class ConsultationsRepository implements IConsultationsRepository {
     constructor(private readonly txHost: TransactionHost<TransactionalAdapterPrisma<PrismaService>>) {}
+
+    async findMany<T extends ConsultationFindManyArgs>(
+        params: SelectSubset<T, ConsultationFindManyArgs>,
+    ): Promise<ConsultationGetPayload<T>[]> {
+        return this.txHost.tx.consultation.findMany(params);
+    }
+
+    async findManyAndCount<T extends ConsultationFindManyArgs>(
+        params: SelectSubset<T, ConsultationFindManyArgs>,
+    ): Promise<FindManyAndCountResult<T>> {
+        return this.txHost.withTransaction(async () => {
+            const consultations = await this.findMany(params);
+            const totalCount = await this.count(params.where);
+
+            return { consultations, totalCount };
+        });
+    }
+
+    async count(whereInput?: ConsultationWhereInput): Promise<number> {
+        return this.txHost.tx.consultation.count({ where: whereInput });
+    }
 
     async create(
         userId: number,
--- server/src/consultations/interfaces/consultations.repository.interface.ts
+++ server/src/consultations/interfaces/consultations.repository.interface.ts
@@ -1,8 +1,21 @@
-import { ConsultationSelect } from "generated/prisma/models";
+import { ConsultationFindManyArgs, ConsultationGetPayload, ConsultationSelect } from "generated/prisma/models";
+import { ConsultationWhereInput, SelectSubset } from "generated/prisma/internal/prismaNamespace";
 import { CreateConsultationDto } from "../dtos/requests/create-consultation.dto";
 import { IConsultation } from "./consultations.interface";
 
+export type FindManyAndCountResult<T extends ConsultationFindManyArgs> = {
+    consultations: ConsultationGetPayload<T>[];
+    totalCount: number;
+};
+
 export interface IConsultationsRepository {
+    findMany<T extends ConsultationFindManyArgs>(
+        params: SelectSubset<T, ConsultationFindManyArgs>,
+    ): Promise<ConsultationGetPayload<T>[]>;
+    findManyAndCount<T extends ConsultationFindManyArgs>(
+        params: SelectSubset<T, ConsultationFindManyArgs>,
+    ): Promise<FindManyAndCountResult<T>>;
+    count(whereInput?: ConsultationWhereInput): Promise<number>;
     create(
         userId: number,
         createConsultationDto: CreateConsultationDto,
